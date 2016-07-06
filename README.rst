4cf1b0e4adc992174cc7d530d58c83454f0843ef
Dave Hunt <dave.hunt@gmail.com>
2016-07-06 13:22:02 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,45 @@
+pytest-mozlog
+=============
+
+pytest-mozlog is a plugin for pytest_ that provides an additional logging
+specialized the Mozilla universe.
+
+.. image:: https://img.shields.io/badge/license-MPL%202.0-blue.svg
+   :target: https://github.com/davehunt/pytest-mozlog/blob/master/LICENSE
+   :alt: License
+.. image:: https://img.shields.io/travis/davehunt/pytest-mozlog.svg
+   :target: https://travis-ci.org/davehunt/pytest-mozlog/
+   :alt: Travis
+.. image:: https://img.shields.io/github/issues-raw/davehunt/pytest-mozlog.svg
+   :target: https://github.com/davehunt/pytest-mozlog/issues
+   :alt: Issues
+.. image:: https://img.shields.io/requires/github/davehunt/pytest-mozlog.svg
+   :target: https://requires.io/github/davehunt/pytest-mozlog/requirements/?branch=master
+   :alt: Requirements
+
+Requirements
+------------
+
+You will need the following prerequisites in order to use pytest-mozlog:
+
+- Python 2.6, 2.7, 3.3, 3.4, 3.5, PyPy, or PyPy3
+- py.test 2.8 or newer
+
+Installation
+------------
+
+To install pytest-mozlog:
+
+.. code-block:: bash
+
+  $ pip install pytest-mozlog
+
+Resources
+---------
+
+- `Issue Tracker`_
+- Code_
+
+.. _pytest: http://www.python.org/
+.. _Issue Tracker: http://github.com/davehunt/pytest-mozlog/issues
+.. _Code: http://github.com/davehunt/pytest-mozlog
---
+++ pytest_mozlog/plugin.py
@@ -0,0 +1,77 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import mozlog
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('mozlog')
+
+    for name, (_class, _help) in mozlog.commandline.log_formatters.iteritems():
+        group.addoption('--log-{0}'.format(name), action='append', help=_help)
+
+    formatter_options = mozlog.commandline.fmt_options.iteritems()
+    for name, (_class, _help, formatters, action) in formatter_options:
+        for formatter in formatters:
+            if formatter in mozlog.commandline.log_formatters:
+                group.addoption(
+                    '--log-{0}-{1}'.format(formatter, name),
+                    action=action,
+                    help=_help)
+
+
+def pytest_configure(config):
+    if not hasattr(config, 'slaveinput'):
+        config.pluginmanager.register(MozLog())
+
+
+class MozLog(object):
+
+    def __init__(self):
+        self.results = {}
+
+    def pytest_configure(self, config):
+        args = {}
+        formatters = mozlog.commandline.log_formatters.iteritems()
+        for name, (_class, _help) in formatters:
+            argname = 'log_{0}'.format(name)
+            if config.getoption(argname):
+                args[argname] = config.getoption(argname)
+
+        formatter_options = mozlog.commandline.fmt_options.iteritems()
+        for name, (_class, _help, formatters, action) in formatter_options:
+            for formatter in formatters:
+                if formatter in mozlog.commandline.log_formatters:
+                    argname = 'log_{0}_{1}'.format(formatter, name)
+                    if config.getoption(argname):
+                        args[argname] = config.getoption(argname)
+
+        mozlog.commandline.setup_logging('pytest', args)
+        self.logger = mozlog.get_default_logger(component='pytest')
+
+    def pytest_sessionstart(self, session):
+        self.logger.suite_start(getattr(self, 'tests', []))
+
+    def pytest_sessionfinish(self, session, exitstatus):
+        self.logger.suite_end()
+
+    def pytest_runtest_logstart(self, nodeid, location):
+        self.logger.test_start(nodeid)
+
+    def pytest_runtest_logreport(self, report):
+        test = report.nodeid
+        status = expected = 'PASS'
+        if hasattr(report, 'wasxfail'):
+            expected = 'FAIL'
+            if report.skipped:
+                status = 'FAIL'
+        elif report.failed:
+            status = 'FAIL' if report.when == 'call' else 'ERROR'
+        elif report.skipped:
+            status = expected = 'SKIP'
+        if status != expected:
+            self.results[test] = (status, expected)
+        if report.when == 'teardown':
+            status, expected = self.results.get(test, ('PASS', 'PASS'))
+            self.logger.test_end(test, status, expected)
